b57566a855b9ac776c74ed37c9657df2f3f137dd
Ekaterina Moreva <moreva.ei@edu.spbstu.ru>
2020-02-09 20:28:08 +0300

    style edits

--- src/js/actions/changeItemText.action.ts
+++ src/js/actions/changeItemText.action.ts
@@ -1,14 +1,10 @@
 import { todosModel } from '../models/todosModel';
 import httpService from '../services/httpService';
 
-export function changeTodoItemTextStatusAction(
-    todoItemId: number,
-    text: string,
-) {
+export function changeTodoItemTextStatusAction(todoItemId: number, text: string,)
+{
     const item = todosModel.items.find(item => item.id === todoItemId);
-
     item.text = text;
-
     httpService.request(
         'PUT',
         `http://localhost:3000/todos/${todoItemId}`,
